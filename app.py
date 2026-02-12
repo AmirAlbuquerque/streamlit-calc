68a19963f83de75ea16facbe0feb5b2e9caa4b2b
Amir Albuquerque <amirsilva00an@gmail.com>
2026-02-12 09:08:43 -0300

    Corrigindo erro app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Calculadora", page_icon="🧮", layout="centered"
 st.title("🧮 Calculadora simples")
 st.caption("Main: soma. Develop: soma + multiplicação.")
 
-col1, col2, col3 = st.columns(2)
+col1, col2 = st.columns(2)
 with col1:
     a = st.number_input("A", value=0.0, step=1.0)
 with col2:
